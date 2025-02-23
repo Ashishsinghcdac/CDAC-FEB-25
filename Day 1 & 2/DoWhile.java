e21ef244253018565ead009a813c098c913bbdb8
Ashishsinghcdac <ashish.singhcdac@gmail.com>
2025-02-23 20:37:16 +0530

    Add files via upload

---
+++ Day 1 & 2/DoWhile.java
@@ -0,0 +1,36 @@
+class DoWhile{
+
+public static void main(String[] args){
+int a=10;
+do{
+System.out.println("DoWhile");
+a--;
+}
+while(a>=5);
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
